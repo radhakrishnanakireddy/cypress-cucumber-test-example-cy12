5e6e21e605a5fb9f6b7cd36fe97ae6417fe60666
Burak Kaygusuz <kaygusuzburak@gmail.com>
2020-07-08 13:52:26 +0300

    signInButton fixed

--- cypress/integration/step_definitions/signIn_with_empty_value.js
+++ cypress/integration/step_definitions/signIn_with_empty_value.js
@@ -1,12 +1,13 @@
-import {Then, When} from "cypress-cucumber-preprocessor/steps";
+import { Then, When } from "cypress-cucumber-preprocessor/steps";
 
 When(/^I click Sign in button$/, () => {
 
-    cy.get('.nav-action-inner')
-        .as(`signInButton`);
+    cy.get('#nav-signin-tooltip > .nav-action-button > .nav-action-inner')
+    .as(`signInButton`);
 
     cy.get(`@signInButton`).click()
 });
+
 When(/^I click Continue button$/, () => {
 
     cy.get(`.a-button-inner > #continue`)
@@ -14,6 +15,7 @@ When(/^I click Continue button$/, () => {
 
     cy.get(`@continueButton`).click()
 });
+
 Then(/^I should see "([^"]*)" message$/, (message) => {
 
     cy.get(`#auth-email-missing-alert > .a-box-inner > .a-alert-content`)
