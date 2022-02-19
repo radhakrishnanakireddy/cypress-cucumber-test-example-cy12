62ee60bdc261eff495be233790fcb60582223674
Burak Kaygusuz <kaygusuzburak@gmail.com>
2022-02-19 11:53:47 +0300

    Remove scenario

--- cypress/integration/step_definitions/search-product/search-product-with-model-number.js
+++
@@ -1,8 +0,0 @@
-import { Then } from "cypress-cucumber-preprocessor/steps";
-
-Then(/^"([^"]*)" should be listed$/, (productName) => {
-  cy.get(`.a-size-medium`).as(`productLinkText`);
-  cy.get(`@productLinkText`).then(($productLinkText) => {
-    expect($productLinkText.text()).contains(productName);
-  });
-});
