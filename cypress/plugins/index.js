9849d0b0754fe81dbba374a26fbc5c46a73e0361
Burak Kaygusuz <kaygusuzburak@gmail.com>
2022-05-17 09:50:53 +0300

    Fix setup and config

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,8 +1,18 @@
 const createBundler = require('@bahmutov/cypress-esbuild-preprocessor');
 const { createEsbuildPlugin } = require('@badeball/cypress-cucumber-preprocessor/esbuild');
+const { beforeRunHook, afterRunHook } = require('cypress-mochawesome-reporter/lib');
 
 module.exports = (on, config) => {
-  require('cypress-mochawesome-reporter/plugin')(on);
+  on('before:run', async (details) => {
+    console.log('override before:run');
+    await beforeRunHook(details);
+  });
+
+  on('after:run', async () => {
+    console.log('override after:run');
+    await afterRunHook();
+  });
+
   const bundler = createBundler({
     plugins: [createEsbuildPlugin(config)],
   });
