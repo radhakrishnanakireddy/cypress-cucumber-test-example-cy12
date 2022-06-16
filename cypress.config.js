63790ffb4ea6fef88fae1e3e21bb20195e1ba4ab
Burak Kaygusuz <kaygusuzburak@gmail.com>
2022-06-16 16:29:33 +0300

    Remove cypress-mochawesome-reporter

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,6 @@
 const { defineConfig } = require('cypress');
 const createBundler = require('@bahmutov/cypress-esbuild-preprocessor');
 const { createEsbuildPlugin } = require('@badeball/cypress-cucumber-preprocessor/esbuild');
-const { beforeRunHook, afterRunHook } = require('cypress-mochawesome-reporter/lib');
 
 module.exports = defineConfig({
   viewportWidth: 1920,
@@ -9,25 +8,8 @@ module.exports = defineConfig({
   defaultCommandTimeout: 20000,
   chromeWebSecurity: false,
   fixturesFolder: false,
-  reporter: 'cypress-mochawesome-reporter',
-  reporterOptions: {
-    reportDir: 'cypress/report',
-    charts: true,
-    reportPageTitle: 'Cypress Cucumber Test Example',
-    overwrite: true,
-  },
   e2e: {
     setupNodeEvents(on, config) {
-      on('before:run', async (details) => {
-        console.log('override before:run');
-        await beforeRunHook(details);
-      });
-
-      on('after:run', async () => {
-        console.log('override after:run');
-        await afterRunHook();
-      });
-
       const bundler = createBundler({
         plugins: [createEsbuildPlugin(config)],
       });
